5c0db85dbf9e012c11cc179e5efb9449bbf02c23
Hging <hging3@gmail.com>
2016-12-09 16:40:04 +0800

    coming soon

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <hello></hello>
   </div>
 </template>
@@ -17,12 +16,16 @@ export default {
 </script>
 
 <style>
-#app {
+  body {
+    background: url(./assets/coming-soon.jpg);
+    background-size: 100%;
+  }
+  #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  color: #fff;
+  }
+
 </style>
